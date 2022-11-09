4f979bd813f1f7c6981029c89dedcf6f28608773
Cristi Gard <65028018+cristigard@users.noreply.github.com>
2022-11-10 00:26:11 +0200

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -7,3 +7,6 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     # generate a unique username every time a user is registered
     username = models.CharField(default=uuid.uuid4(), unique=True, max_length = 120)
+    
+    def __str__(self):
+        return f'{self.email}'
